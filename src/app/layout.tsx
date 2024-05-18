d8d6465a6e853b2c703c4404d0d31b1e45f707c7
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-05-19 03:02:38 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
-import "react-loading-skeleton/dist/skeleton.css"
+import "react-loading-skeleton/dist/skeleton.css";
+import 'simplebar-react/dist/simplebar.min.css'
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <Providers>
       <body className={inter.className}>
+        <Toaster/>
         <Navbar/>
         {children}</body>
         </Providers>
