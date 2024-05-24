ca358cfbef50fd33908017ba990ac87e377be1ba
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-05-25 00:29:33 +0530

    .

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,10 +1,11 @@
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(
   request: NextRequest,
   { params }: any
-) {
+): Promise<void | NextResponse<unknown>> {
   const endpoint = params.kindeAuth
-  return handleAuth(request, endpoint)
+  await handleAuth(request, endpoint)
+  return new NextResponse(null, { status: 200 }) // Or any appropriate response
 }
