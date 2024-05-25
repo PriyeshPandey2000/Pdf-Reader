08c0a0d274d47c58055a768a5b8a4d2d47f90d7e
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-05-26 00:12:12 +0530

    .

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,12 +1,16 @@
-import { PineconeClient } from '@pinecone-database/pinecone'
+import { Pinecone } from '@pinecone-database/pinecone';
 
 export const getPineconeClient = async () => {
-  const client = new PineconeClient()
+  const client = new Pinecone(
+    {
+      apiKey: process.env.PINECONE_API_KEY!
+  }
+  )
 
-  await client.init({
-    apiKey: process.env.PINECONE_API_KEY!,
-    environment: 'us-east1-gcp',
-  })
+  // await client.init({
+  //   apiKey: process.env.PINECONE_API_KEY!,
+  //   environment: 'us-east1-gcp',
+  // })
 
   return client
 }
